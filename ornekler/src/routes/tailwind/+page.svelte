d303dfc3124fc6a0947064bc3d435f653f6eaaa6
Yavuz Tanrıverdi <sternix@gmail.com>
2024-06-12 15:35:22 +0300

    tailwind button eklendi

--- ornekler/src/routes/tailwind/+page.svelte
+++ ornekler/src/routes/tailwind/+page.svelte
@@ -1,1 +1,7 @@
 <h1>Tailwind örnekleri</h1>
+<a href="/">Anasayfa</a>
+<nav>
+    <ul>
+        <li><a href="/tailwind/button">Login</a></li>
+    </ul>
+</nav>
---
+++ ornekler/src/routes/tailwind/button/+page.svelte
@@ -0,0 +1,5 @@
+<a href="/tailwind/button" class="bg-green-400 btn">Test</a>
+<a href="/tailwind/button" class="bg-blue-300 btn">Test</a>
+<a href="/tailwind/button" class="bg-red-300 btn">Test</a>
+<a href="/tailwind/button" class="bg-yellow-300 btn">Test</a>
+<a href="/tailwind/button" class="bg-yellow-300 btn">Test</a>
