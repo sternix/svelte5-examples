937f84bd2ad694dabcc88ae5bdf05009bacaccc0
Yavuz Tanrıverdi <sternix@gmail.com>
2024-07-04 17:02:56 +0300

    keypad örneği eklendi

--- temeller/src/routes/+page.svelte
+++ temeller/src/routes/+page.svelte
@@ -1,12 +1,27 @@
 <h3>Svelte Temel örnekleri</h3>
+
+<a href="/">Anasayfa</a>
 <nav>
-    <a href="./runes">Runes</a>
-    <a href="./snippet">Snippets</a>
-    <a href="./clock">Saat</a>
-    <a href="./flight">Flight Booker</a>
-    <a href="./hackernews">Hackernews</a>
-    <a href="./tweens">Tweened</a>
-    <a href="./springs">Springs</a>
-    <a href="./transition">Transition</a>
-    <a href="./action">Action</a>
+    <ul>
+        <li><a href="./runes">Runes</a></li>
+        <li><a href="./snippet">Snippets</a></li>
+        <li><a href="./clock">Saat</a></li>
+        <li><a href="./flight">Flight Booker</a></li>
+        <li><a href="./hackernews">Hackernews</a></li>
+        <li><a href="./tweens">Tweened</a></li>
+        <li><a href="./springs">Springs</a></li>
+        <li><a href="./transition">Transition</a></li>
+        <li><a href="./action">Action</a></li>
+        <li><a href="./keypad">Keypad</a></li>
+    </ul>
 </nav>
+
+<style>
+    * {
+        padding: 0;
+    }
+
+    ul {
+        list-style: none;
+    }
+</style>
---
+++ temeller/src/routes/keypad/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Keypad from "./Keypad.svelte";
+
+    let pin = $state("");
+    let view = $derived(pin ? pin.replace(/\d(?!$)/g, "•") : "enter your pin");
+
+    function handleSubmit() {
+        alert(`submitted ${pin}`);
+    }
+</script>
+
+<h1 style="opacity: {pin ? 1 : 0.4}">
+    {view}
+</h1>
+
+<Keypad bind:value={pin} submit={handleSubmit} />
