221d447a27aa64402ec42f268b93c41741624c8b
Yavuz Tanrıverdi <sternix@gmail.com>
2024-08-06 09:51:20 +0300

    event değiştirme örneği eklendi

---
+++ temeller/src/routes/events/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    // https://github.com/sveltejs/svelte/blob/main/packages/svelte/tests/runtime-runes/samples/event-attribute-not-reactive/main.svelte
+
+    let count = $state(0);
+    const handlers = {
+        current: increment,
+    };
+    function increment() {
+        count += 1;
+    }
+    function decrement() {
+        count -= 1;
+    }
+</script>
+
+<button onclick={() => (handlers.current = increment)}>increment</button>
+<button onclick={() => (handlers.current = decrement)}>decrement</button>
+<button onclick={handlers.current}>clicks: {count}</button>
